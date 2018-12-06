1a51eba3b74c203bfa57fdf8a0f552409002bb03
h <37706103+helunxing@users.noreply.github.com>
2018-12-06 21:17:18 +0800

    155应该是做对了，感觉判题程序有问题。

---
+++ leetcode/lt155.py
@@ -0,0 +1,58 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_s = []
+        self.min = 0
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if len(self.stack) == 0:
+            self.min = x
+        else:
+            if x < self.min:
+                self.min = x
+        self.stack.append(x)
+        self.min_s.append(min)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        if len(self.stack) > 0:
+            del self.stack[-1]
+            del self.min_s[-1]
+        # else:
+        #     throw
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if len(self.stack) > 0:
+            return self.stack[-1]
+        else:
+            return 0
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if len(self.stack) > 0:
+            return self.min_s[-1]
+        else:
+            return 0
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
