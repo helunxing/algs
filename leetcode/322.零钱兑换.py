af4dac4b8696ef7b37c842786dfb79d9510d2cb1
h <37706103+helunxing@users.noreply.github.com>
2019-06-21 18:39:24 +0800

    重命名

--- leetcode/322.零钱兑换.py
+++ leetcode/322.零钱兑换.py
@@ -5,6 +5,26 @@
 #
 
 
+class Solution_old(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [-1]*(amount+1)
+        dp[0] = 0
+
+        for i in range(amount+1):
+            for j in coins:
+                if dp[i] != -1 and i+j <= amount:
+                    dp[i+j] = dp[i]+1 if \
+                        dp[i]+1 < dp[i+j] or dp[i+j] == -1 \
+                        else dp[i+j]
+
+        return dp[amount]
+
+
 class Solution_1905(object):
     def coinChange(self, coins, amount):
         """
